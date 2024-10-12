986ee8e0fd74566b2b3dc49c2f38279ed903aa39
Yasir Mehboob <115621928+fadyxnoob@users.noreply.github.com>
2024-10-12 22:27:48 +0500

    add to gh-pages

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Navbar, Topbar, Banner, ProductCard, LiveStream, Counter, PostCard, WinnerCard, Footer } from '../../index'
-
+import Product1 from '../../assets/images/product1.jpg'
+import Product2 from '../../assets/images/product2.jpg'
+import Product3 from '../../assets/images/product3.jpg'
+import Blog1 from '../../assets/images/blog1.jpg'
+import Blog2 from '../../assets/images/blog2.jpg'
+import Blog3 from '../../assets/images/blog3.jpg'
+import Candidate3 from '../../assets/images/candi3.jpg'
+import Candidate7 from '../../assets/images/candi7.jpg'
+import Candidate9 from '../../assets/images/candi9.jpg'
 const Home = () => {
     return (
         <main className='overflow-hidden'>
@@ -19,9 +27,9 @@ const Home = () => {
                    productCardContainer
                     ">
 
-                    <ProductCard image='src/assets/images/product1.jpg' alt='ProductOne'   />
-                    <ProductCard image='src/assets/images/product2.jpg' alt='ProductTwo'   />
-                    <ProductCard image='src/assets/images/product3.jpg' alt='ProductThree' />
+                    <ProductCard image={Product1} alt='ProductOne'   />
+                    <ProductCard image={Product2} alt='ProductTwo'   />
+                    <ProductCard image={Product3} alt='ProductThree' />
                 </div>
             </div>
 
@@ -44,9 +52,9 @@ const Home = () => {
                    sm:items-center sm:justify-center
                    productCardContainer
                     ">
-                    <PostCard image='src/assets/images/blog1.jpg' alt='Blog1' user='yasir' cat='results' day='29' month='Oct' title='Get Ready for Gamming Glory : Announcing the Next Tournament' />
-                    <PostCard image='src/assets/images/blog2.jpg' alt='Blog2' user='hammad' cat='tournament' day='31' month='Nov' title='Mastering the Battlegrounds : Your Ultimate PUBG Blog' />
-                    <PostCard image='src/assets/images/blog3.jpg' alt='Blog3' user='yasir' cat='results' day='19' month='Mar' title='Recapping the Thrills and Spills : ELDRITCH S3 Tournament Results Unveiled ' />
+                    <PostCard image={Blog1} alt='Blog1' user='yasir' cat='results' day='29' month='Oct' title='Get Ready for Gamming Glory : Announcing the Next Tournament' />
+                    <PostCard image={Blog2} alt='Blog2' user='hammad' cat='tournament' day='31' month='Nov' title='Mastering the Battlegrounds : Your Ultimate PUBG Blog' />
+                    <PostCard image={Blog3} alt='Blog3' user='yasir' cat='results' day='19' month='Mar' title='Recapping the Thrills and Spills : ELDRITCH S3 Tournament Results Unveiled ' />
                 </div>
 
             </div>
@@ -62,9 +70,9 @@ const Home = () => {
                     sm:items-center justify-center
                     productCardContainer
                     ">
-                    <WinnerCard image='src/assets/images/candi3.jpg' alt='Final Winner' />
-                    <WinnerCard image='src/assets/images/candi7.jpg' alt='Final Winner' />
-                    <WinnerCard image='src/assets/images/candi9.jpg' alt='Final Winner' />
+                    <WinnerCard image={Candidate3} alt='Final Winner' />
+                    <WinnerCard image={Candidate7} alt='Final Winner' />
+                    <WinnerCard image={Candidate9} alt='Final Winner' />
                 </div>
 
             </div>
