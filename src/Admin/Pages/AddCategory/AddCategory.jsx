6f9848a5e03de4c7de0e2baefcd4657c4016133d
Yasir Mehboob <115621928+fadyxnoob@users.noreply.github.com>
2024-12-03 22:39:32 +0500

    minor changes

--- src/Admin/Pages/AddCategory/AddCategory.jsx
+++ src/Admin/Pages/AddCategory/AddCategory.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../Components/Button/Button'
+import { useNavigate } from 'react-router-dom'
+import DatabaseService from '../../Appwrite/Database'
+
 
 const AddCategory = () => {
+    const [category, setCategory] = useState({ title: '', option: '' })
+    const navigate = useNavigate();
 
-    const options = ['post', 'product']
     return (
         <>
             <h1 className='px-2'>Add Category</h1>
@@ -15,11 +19,15 @@ const AddCategory = () => {
                             id='catName'
                             type="text"
                             className='mt-2 w-full bg-[#e8f0fe] focus:border-b-2 outline-none border-primary h-10 px-2'
+                            value={category.title}
+                            onChange={(e) => setCategory((prev) => ({ ...prev, title: e.target.value }))}
                         />
                     </div>
                     <div className="w-full">
-                    <label htmlFor="type">Type</label> <br />
-                        <select name="" id="type"
+                        <label htmlFor="type">Type</label> <br />
+                        <select
+                            onChange={(e) => setCategory((prev) => ({ ...prev, option: e.target.value }))}
+                            id="type"
                             className='mt-2 w-full outline-none focus:border-b-2 border-primary bg-[#e8f0fe] px-2 h-10'
                         >
                             <option value="post" className='h-10 p-2'>Post</option>
@@ -27,7 +35,7 @@ const AddCategory = () => {
                         </select>
                     </div>
                 </div>
-                <Button title={'Add Category'}  style={'mt-5 px-5'}/>
+                <Button title={'Add Category'} style={'mt-5 px-5'} />
             </div>
         </>
     )
