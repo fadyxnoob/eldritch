e5350e458d11c6382d4c4d6e3cfe28fb61233cc7
Yasir Mehboob <115621928+fadyxnoob@users.noreply.github.com>
2024-12-10 00:50:01 +0500

    responsive completed

--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -42,7 +42,7 @@ const MyCart = () => {
 
     const handleRemoveItem = (id) => {
         dispatch(removeItem({ id }))
-        setAlert({type:'info', message:'Item removed from the cart.'})
+        setAlert({ type: 'info', message: 'Item removed from the cart.' })
 
     }
 
@@ -61,79 +61,78 @@ const MyCart = () => {
                     onClose={() => setAlert(null)}
                 />
             )}
-            <table className='rounded boxShadow w-[70%] border mx-auto'>
-                <thead>
-                    <tr>
-                        <th colSpan={6}>
-                            <h1 className='p-0 m-0 bg-primary text-light text-center text-2xl py-2'>My Cart</h1>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th className='border p-2 font-light'>#</th>
-                        <th className='border p-2 font-light'>Product</th>
-                        <th className='border p-2 font-light'>Price</th>
-                        <th className='border p-2 font-light'>Quantity</th>
-                        <th className='border p-2 font-light'>Total</th>
-                        <th className='border p-2 font-light'>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        products.map((pro, index) => {
-                            return (<tr key={pro.id}>
-                                <td className='text-center border font-light'>{index + 1}</td>
-                                <td className='text-center border font-light'>{pro.name}</td>
-                                <td className='text-center border font-light'>${pro.price}</td>
-                                <td className='text-center border font-light'>
-                                    <div className='flex  h-6 items-center justify-between'>
-                                        <button onClick={() => handleMinusQuantity(pro.id, pro.quantity)} >
-                                            <CiSquareMinus className='size-7 text-primary cursor-pointer' />
-                                        </button>
-                                        <div className=''>{pro.quantity}</div>
-                                        <button onClick={() => handlePlusQuantity(pro.id, pro.quantity)}>
-                                            <CiSquarePlus className='size-7 text-primary cursor-pointer' />
-                                        </button>
-                                    </div>
-                                </td>
-
-                                <td className='text-center border font-light'>${pro.quantity * pro.price}</td>
-                                <td className='text-center border font-light'>
-                                    <button
-                                        onClick={() => handleRemoveItem(pro.id)}
-                                    >
-                                        <CiCircleRemove className='text-primary size-8 mx-auto cursor-pointer' />
-                                    </button>
-                                </td>
-                            </tr>)
-                        })
-                    }
-
-                </tbody>
-                <tfoot>
-                    <tr className='bg-primary text-light'>
-                        <td className='text-center border p-2 font-light'>Total Products</td>
-                        <td className='text-center border p-2 font-light'>{totalItems}</td>
-                        <td className='text-center border p-2 font-light'>Total Items</td>
-                        <td className='text-center border p-2 font-light'>{totalQuantity}</td>
-                        <td className='text-center border p-2 font-light'>Total Price</td>
-                        <td className='text-center border p-2 font-light'>${totalPrice}</td>
-                    </tr>
-                </tfoot>
-            </table>
-
-            <div className="mt-20 w-[80%] mx-auto">
+            <div className="overflow-x-auto">
+                <table className='rounded boxShadow w-full sm:w-[70%] border mx-auto overflow-auto bg-green-200'>
+                    <thead>
+                        <tr>
+                            <th colSpan={6}>
+                                <h1 className='p-0 m-0 bg-primary text-light text-center text-2xl py-2'>My Cart</h1>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th className='border p-2 font-light'>#</th>
+                            <th className='border p-2 font-light'>Product</th>
+                            <th className='border p-2 font-light'>Price</th>
+                            <th className='border p-2 font-light'>Quantity</th>
+                            <th className='border p-2 font-light'>Total</th>
+                            <th className='border p-2 font-light'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products.map((pro, index) => {
+                                return (
+                                    <tr key={pro.id}>
+                                        <td className='text-center border font-light'>{index + 1}</td>
+                                        <td className='text-center border font-light min-w-[300px]'>{pro.name}</td>
+                                        <td className='text-center border font-light'>${pro.price}</td>
+                                        <td className='text-center border font-light'>
+                                            <div className='flex h-6 items-center justify-between'>
+                                                <button onClick={() => handleMinusQuantity(pro.id, pro.quantity)} >
+                                                    <CiSquareMinus className='size-7 text-primary cursor-pointer' />
+                                                </button>
+                                                <div>{pro.quantity}</div>
+                                                <button onClick={() => handlePlusQuantity(pro.id, pro.quantity)}>
+                                                    <CiSquarePlus className='size-7 text-primary cursor-pointer' />
+                                                </button>
+                                            </div>
+                                        </td>
+                                        <td className='text-center border font-light'>${pro.quantity * pro.price}</td>
+                                        <td className='text-center border font-light'>
+                                            <button onClick={() => handleRemoveItem(pro.id)}>
+                                                <CiCircleRemove className='text-primary size-8 mx-auto cursor-pointer' />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr className='bg-primary text-light'>
+                            <td className='text-center border p-2 font-light'>Total Products</td>
+                            <td className='text-center border p-2 font-light'>{totalItems}</td>
+                            <td className='text-center border p-2 font-light'>Total Items</td>
+                            <td className='text-center border p-2 font-light'>{totalQuantity}</td>
+                            <td className='text-center border p-2 font-light'>Total Price</td>
+                            <td className='text-center border p-2 font-light'>${totalPrice}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <div className="mt-20 w-full px-2 mx-auto sm:w-5/6">
                 <h2 className='text-center text-4xl'>Order Now</h2>
                 <div className='p-5 boxShadow mt-5'>
                     <form method='POST'>
-                        <div className='flex justify-between gap-5'>
-                            <div className='w-1/2'>
+                        <div className='flex justify-between gap-5 flex-col sm:flex-row'>
+                            <div className='w-full sm:w-1/2'>
                                 <label htmlFor="name">Name</label>
                                 <input
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
                                     id='name' type="text" placeholder='Enter name' className='outline-none focus:border-b-2 border-primary w-full bg-[#e8f0fe] h-10 px-2' />
                             </div>
-                            <div className='w-1/2'>
+                            <div className='w-full sm:w-1/2'>
                                 <label htmlFor="email">Email Address</label>
                                 <input
                                     value={email}
@@ -141,15 +140,15 @@ const MyCart = () => {
                                     id='email' type="email" placeholder='i.e example@example.com' className='outline-none focus:border-b-2 border-primary w-full bg-[#e8f0fe] h-10 px-2' />
                             </div>
                         </div>
-                        <div className='flex justify-between gap-5 mt-5'>
-                            <div className='w-1/2'>
+                        <div className='flex justify-between gap-5 w-full flex-col sm:flex-row'>
+                            <div className='w-full sm:w-1/2'>
                                 <label htmlFor="phoneNo">Phone No</label>
                                 <input
                                     value={phone}
                                     onChange={(e) => setPhone(e.target.value)}
                                     id='phoneNo' type="text" placeholder='i.e +9213587846546' className='outline-none focus:border-b-2 border-primary w-full bg-[#e8f0fe] h-10 px-2' />
                             </div>
-                            <div className='w-1/2'>
+                            <div className='w-full sm:w-1/2'>
                                 <label htmlFor="postalCode">Postal Code</label>
                                 <input
                                     value={postalCode}
